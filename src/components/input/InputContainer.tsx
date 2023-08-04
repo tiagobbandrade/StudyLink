7f2734de106c725f1d250bd15c8129f6a5b97fb0
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-08-04 18:48:38 -0300

    style: change the input focused background color

--- src/components/input/InputContainer.tsx
+++ src/components/input/InputContainer.tsx
@@ -16,7 +16,7 @@ export default function InputContainer({
       className={`w-full flex items-start justify-start flex-col ${
         error
           ? "[&>input]:border-red-500 [&>input]:shadow-red-300 [&>input]:text-red-500 [&>input]:placeholder:text-red-500 [&>input]:focus-within:bg-red-50 [&>input]:focus-within:bg-opacity-80 [&>label]:text-red-500 [&>label]:shadow-red-200"
-          : "[&>input]:text-zinc-500 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-100 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
+          : "[&>input]:text-zinc-500 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-200 [&>input]:focus-within:bg-opacity-50 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
       }`}
     >
       {children}
