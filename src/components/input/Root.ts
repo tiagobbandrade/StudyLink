61ed386520f3d301950a281b99126e98a729ba8a
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-08-04 12:45:14 -0300

    feat: add root inputs components

--- src/components/input/Root.ts
+++ src/components/input/Root.ts
@@ -1,9 +1,13 @@
 import Input from "./Input";
 import InputContainer from "./InputContainer";
+import InputErrorMessage from "./InputErrorMessage";
 import InputLabel from "./InputLabel";
+import InputRequiredAlert from "./InputRequiredAlert";
 
-const InputRoot = {
+export const InputRoot = {
   Container: InputContainer,
   Input: Input,
   Label: InputLabel,
+  Required: InputRequiredAlert,
+  ErrorMessage: InputErrorMessage,
 };
