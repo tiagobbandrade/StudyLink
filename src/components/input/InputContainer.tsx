4b1f6b57b046f5c9da5f05b16f6b0a1faffbb4ed
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-08-04 23:26:59 -0300

    refactor: remove unused tailwind classes

--- src/components/input/InputContainer.tsx
+++ src/components/input/InputContainer.tsx
@@ -15,8 +15,8 @@ export default function InputContainer({
       {...props}
       className={`w-full flex items-start justify-start flex-col ${
         error
-          ? "[&>input]:border-red-500 [&>input]:shadow-red-300 [&>input]:text-red-500 [&>input]:placeholder:text-red-500 [&>input]:focus-within:bg-red-50 [&>input]:focus-within:bg-opacity-80 [&>label]:text-red-500 [&>label]:shadow-red-200"
-          : "[&>input]:text-zinc-500 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-200 [&>input]:focus-within:bg-opacity-50 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
+          ? "[&>input]:border-red-500 [&>input]:bg-red-50 [&>input]:shadow-red-300 [&>input]:text-red-500 [&>input]:placeholder:text-red-500 [&>label]:text-red-500 [&>label]:shadow-red-200"
+          : "[&>input]:text-zinc-500 [&>input]:bg-zinc-50 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-200 [&>input]:focus-within:bg-opacity-50 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
       }`}
     >
       {children}
