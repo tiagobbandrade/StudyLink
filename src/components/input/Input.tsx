1bba57ada85fb7e94fd54aa3bf94e38455e27f2b
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-08-09 21:02:04 -0300

    feat: add tailwind merge function

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,16 +1,25 @@
 import { InputHTMLAttributes, RefObject } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputInterface extends InputHTMLAttributes<HTMLInputElement> {
   error?: string;
   inputRef?: RefObject<HTMLInputElement> | null;
 }
 
-export default function Input({ error, inputRef, ...props }: InputInterface) {
+export default function Input({
+  error,
+  className,
+  inputRef,
+  ...props
+}: InputInterface) {
   return (
     <input
       {...props}
       ref={inputRef}
-      className="w-full text-xs p-3 border rounded-md shadow-sm transition-colors duration-300 focus:outline-none"
+      className={twMerge(
+        "w-full text-xs p-3 border rounded-md shadow-sm transition-colors duration-300 focus:outline-none",
+        className
+      )}
     />
   );
 }
--- src/components/input/InputContainer.tsx
+++ src/components/input/InputContainer.tsx
@@ -1,4 +1,5 @@
 import { HTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface InputContainerInterface extends HTMLAttributes<HTMLDivElement> {
   error?: string;
@@ -8,16 +9,20 @@ interface InputContainerInterface extends HTMLAttributes<HTMLDivElement> {
 export default function InputContainer({
   error,
   children,
+  className,
   ...props
 }: InputContainerInterface) {
   return (
     <div
       {...props}
-      className={`w-full flex items-start justify-start flex-col ${
-        error
-          ? "[&>input]:border-red-500 [&>input]:bg-red-50 [&>input]:shadow-red-300 [&>input]:text-red-500 [&>input]:placeholder:text-red-500 [&>label]:text-red-500 [&>label]:shadow-red-200"
-          : "[&>input]:text-zinc-500 [&>input]:bg-zinc-50 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-200 [&>input]:focus-within:bg-opacity-50 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
-      }`}
+      className={twMerge(
+        `w-full flex items-start justify-start flex-col ${
+          error
+            ? "[&>input]:border-red-500 [&>input]:bg-red-50 [&>input]:shadow-red-300 [&>input]:text-red-500 [&>input]:placeholder:text-red-500 [&>label]:text-red-500 [&>label]:shadow-red-200"
+            : "[&>input]:text-zinc-500 [&>input]:bg-zinc-50 [&>input]:border-zinc-900 [&>input]:shadow-zinc-400 [&>input]:placeholder:text-zinc-500 [&>input]:focus-within:bg-zinc-200 [&>input]:focus-within:bg-opacity-50 [&>label]:text-zinc-800 [&>label]:shadow-zinc-300"
+        }`,
+        className
+      )}
     >
       {children}
     </div>
